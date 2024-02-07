261a5039e5d5bf951632c5b356be7e9eb7f63a43
gowtham <gowthamkumar3217@gmail.com>
2024-02-07 11:02:47 +0530

    second commit

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ class Demo {
     public static void main(String a[]) {
         int j = 5; // local variable must be initialized
         System.out.println(j);
-
+        System.out.println("hecking");
         String str = "hello how are you puja";
         boolean str1 = str.endsWith("ja");
         System.out.println(str1);
